ac687aa416d35da6b5d6f9258626d71dfb9ea628
Caio Nogueira <up201806218@fe.up.pt>
2021-03-21 19:08:18 +0000

    Restructured the code into modules

---
+++ src/piece.py
@@ -0,0 +1,15 @@
+class Center:
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.content = color + "Center"
+
+class Piece:
+    def __init__(self, x, y, destX, destY, color):
+        self.x = x
+        self.y = y
+        self.destX = destX
+        self.destY = destY
+        self.color = color
+        self.content = color + "Piece"
